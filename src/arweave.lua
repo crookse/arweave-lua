f6f21343231b11902841518e4ce27d68dd6c39f5
Eric Crooks <crookse@users.noreply.github.com>
2024-03-10 20:04:17 -0400

    feat: expose arweave.testing.utils

--- src/arweave.lua
+++ src/arweave.lua
@@ -1,6 +1,10 @@
+local testing = require "src.testing"
 local transactions = require "src.transactions"
+
 local mod = {
   version = "0.0.1-3"
+
+  testing = testing,
   transactions = transactions
 }
 
---
+++ src/testing.lua
@@ -0,0 +1,5 @@
+local utils = require "src.testing.utils"
+
+return {
+  utils = utils
+}
