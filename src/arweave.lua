ec3acd54dc0d7b4b86afd73553d5e0740c2031f7
Eric Crooks <crookse@users.noreply.github.com>
2024-03-10 20:06:59 -0400

    chore: bump version to 0.0.1-4

--- src/arweave.lua
+++ src/arweave.lua
@@ -2,7 +2,7 @@ local testing = require "src.testing"
 local transactions = require "src.transactions"
 
 local mod = {
-  version = "0.0.1-3"
+  version = "0.0.1-4",
 
   testing = testing,
   transactions = transactions
